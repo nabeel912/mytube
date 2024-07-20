ea96b3dfa4f9ff791ccff3a2d0e7654909bb03c8
nabeel912 <nabeel912@gmail.com>
2024-07-20 22:10:50 +0300

    new

--- youtube_events.js
+++ youtube_events.js
@@ -3,11 +3,11 @@ var player2;
 var done = false;
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-
-
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
